ff8e73b3728dc00e7c513436db479ec958ae3af9
Andrew E. Shao <aeshao@stellar-intel.princeton.edu>
2023-09-21 11:23:00 -0400

    Changes to work with NZ>2

--- src/parameterizations/lateral/MOM_hor_visc.F90
+++ src/parameterizations/lateral/MOM_hor_visc.F90
@@ -2407,7 +2407,7 @@ subroutine hor_visc_init(Time, G, GV, US, param_file, diag, CS, ADp)
 !    case("forpy")
 !      call forpy_run_python_init(CS%python,trim(CS%python_dir),trim(CS%python_file))
     case("smartsim")
-      call smartsim_run_python_init(CS%smartsim_python,trim(CS%python_dir),trim(CS%python_file))
+      call smartsim_run_python_init(CS%smartsim_python,trim(CS%python_dir),trim(CS%python_file),param_file)
     case default
       call MOM_error(FATAL, "Invalid library selected for language bridging")
     end select
